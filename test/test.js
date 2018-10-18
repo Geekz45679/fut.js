2ba99de98cdd49e5a2b7730dff70a049ef0b7d7a
Brad <bradz.1708@gmail.com>
2018-10-18 22:25:52 +1100

    Add: Added Mocha unit tests for getPrice()

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const getPlayer = require('../src/getPlayer.js');
+const getPrice = require('../src/getPrice.js');
 
 describe('getPlayer(\'Messi\', true)', () => {
   it('Should return a map with multiple keys of data for Messi, in an organised format', async () => {
@@ -19,6 +20,23 @@ describe('getPlayer(\'Messi\', false)', () => {
 describe('getPlayer(\'thisisnotaplayer\', true)', () => {
   it('Shouldn\'t return any data.', async () => {
     const player = await getPlayer('thisisnotaplayer', true);
-    if (player) return assert.fail(`The test returned ${player.size} results(s), zero were expected.`);
+    if (player) return assert.fail(`The test returned ${player.length} results(s), zero were expected.`);
   });
+});
+
+describe('getPrice(\'Messi\')', () => {
+	it('Should return a price above 150 for Xbox.', async () => {
+		const price = await getPrice('158023');
+		if (!price || !price > 150) return assert.fail(`The test returned ${price.xbox} and was meant to return a value above 150.`);
+	});
+
+	it('Should return a price above 150 for PS4.', async () => {
+		const price = await getPrice('158023');
+		if (!price || !price > 150) return assert.fail(`The test returned ${price.ps} and was meant to return a value above 150.`);
+	});
+
+	it('Should return a price above 150 for PC.', async () => {
+		const price = await getPrice('158023');
+		if (!price || !price > 150) return assert.fail(`The test returned ${price.pc} and was meant to return a value above 150.`);
+	});
 });
